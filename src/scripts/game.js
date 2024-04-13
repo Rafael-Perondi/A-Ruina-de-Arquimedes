d87322aa3f21db3b6ec5e80acae83d9df8d01d49
TheRodrig0 <86856395+TheRodrig0@users.noreply.github.com>
2024-04-13 00:08:13 +0000

    Player has been added - This player has animations

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,26 +1,44 @@
 class Test extends Phaser.Scene {
     constructor() {
         super({ key: 'Test' })
+
+        this.player = null
+        this.controls = null
+        this.water = null
+
     }
 
-    preload() {
-        this.load.image("grassImage", "assets/map/grassTileset.png")
-        this.load.image("waterImage", "assets/map/waterTileset.png")
-        this.load.tilemapTiledJSON("map", "assets/map/map.json")
+    preload() { // Carregando as imagens para o phaser
+        this.load.image("grassImage", "assets/map/tiles/grassTileset.png")
+        this.load.image("waterImage", "assets/map/tiles/waterTileset.png")
+        this.load.tilemapTiledJSON("map", "assets/map/map.json") 
+
+        loadPlayerSprites(this)
     }
 
     create() {
-        const map = this.make.tilemap({ key: "map" })
+        const map = this.make.tilemap({ key: "map" }) 
 
         const tilesetGrass = map.addTilesetImage("grass", "grassImage")
         const tilesetWater = map.addTilesetImage("water", "waterImage")
 
-        const grass = map.createLayer("grass", tilesetGrass, 0, 0)
-        const water = map.createLayer("water", tilesetWater, 0, 0)
+        const grass = map.createLayer("grass", tilesetGrass, 0, 0) 
+        this.water = map.createLayer("water", tilesetWater, 0, 0)
+
+        this.water.setCollisionByProperty({ collider: true})
+
+        this.player = createPlayer(this)
+        this.player.anims.play('player_idle', true)
+
+        this.physics.add.collider(this.player, this.water)
+
+        this.controls = createControls(this)
 
     }
 
-    update() {}
+    update() {
+        configControls(this.player, this.controls, this)
+    }
 }
 
 var config = {
@@ -28,7 +46,14 @@ var config = {
     backgroundColor: "#125555",
     width: 960,
     height: 960,
-    scene: Test
+    scene: Test,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: {y: 0}
+        }
+
+    }
 }
 
 
